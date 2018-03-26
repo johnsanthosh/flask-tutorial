74b346bc5e9022e936f27989071b20b0db5041c9
John Santhosh <jsanthos@asu.edu>
2018-03-26 12:18:26 -0700

    Part II: Templates.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'John'}
+    posts = [
+        {
+            'author': {'username': 'Tinu'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Bosco'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
